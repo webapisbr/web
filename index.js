3dd8731ddc0c3b6f314df8caab36d92bbc629852
Mike <korostelevm@gmail.com>
2022-12-22 09:26:10 -0500

    create

--- index.js
+++ index.js
@@ -3,13 +3,39 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 
+const chromium = require('chrome-aws-lambda');
 
 
 app.use('/',router)
 
 router.get('/', async (req, res)=>{
+    let result = null;
+    let browser = null;
+    console.log(await chromium.executablePath)
+    
+    try {
+      browser = await chromium.puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+        ignoreHTTPSErrors: true,
+      });
+  
+      let page = await browser.newPage();
+  
+      await page.goto(event.url || 'https://example.com');
+  
+      result = await page.title();
+    } catch (error) {
+        throw error
+    } finally {
+      if (browser !== null) {
+        await browser.close();
+      }
+    }
 
-    return res.send('ok')
+    return res.send(result)
 })
 
 app.listen(3000)
