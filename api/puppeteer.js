8d1e382c3f0eef25f2cb2ba998391dd27e66ac43
webapisbr <133459926+webapisbr@users.noreply.github.com>
2023-05-15 01:31:34 -0300

    Update puppeteer.js

--- api/puppeteer.js
+++ api/puppeteer.js
@@ -1,11 +1,12 @@
 async function init() {
-    const puppeteer = require('chrome-aws-lambda');
-    const browser = await puppeteer.puppeteer.launch({
-        executablePath: './node_modules/chromium/lib/chromium/chrome-linux/chrome',
-        args: puppeteer.args,
-        defaultViewport: puppeteer.defaultViewport,
-        executablePath: await puppeteer.executablePath,
-        headless: puppeteer.headless,
+    const chromium = require("@sparticuz/chromium");
+    const puppeteer = require("puppeteer-core");
+    //const puppeteer = require('chrome-aws-lambda');
+    const browser = await puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath(),
+        headless: chromium.headless,
         ignoreHTTPSErrors: true
     });
     return browser;
