ce70f65e85cb9cc95b42197be51760dbb2ee80af
webapisbr <133459926+webapisbr@users.noreply.github.com>
2023-05-14 23:27:22 -0300

    Update puppeteer.js

--- api/puppeteer.js
+++ api/puppeteer.js
@@ -1,11 +1,9 @@
 async function init() {
     const puppeteer = require('chrome-aws-lambda');
-    const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
-    const executablePath = await puppeteer.executablePath || LOCAL_CHROME_EXECUTABLE;
     const browser = await puppeteer.puppeteer.launch({
         args: puppeteer.args,
         defaultViewport: puppeteer.defaultViewport,
-        executablePath: executablePath,
+        executablePath: await puppeteer.executablePath,
         headless: puppeteer.headless,
         ignoreHTTPSErrors: true
     });
